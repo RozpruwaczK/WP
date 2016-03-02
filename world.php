85cc89ffe8f6ed58ba1adf08c86883aa9e557910
RozpruwaczK <spamlapacz+github@gmail.com>
2016-03-03 00:03:25 +0100

    Added files via upload

---
+++ world.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="content-type" CONTENT="text/html; charset=iso-8859-2">
+<link rel="stylesheet" type="text/css" href="world.css"/>
+<link rel="stylesheet" type="text/css" href="item-image.css"/>
+</HEAD>
+<BODY>
+	<div>
+		<ul>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+		</ul>
+		<div class="clear"></div>
+		
+		<ul>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+		</ul>
+		<div class="clear"></div>
+		<ul>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+		</ul>
+		<div class="clear"></div>
+		<ul>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+			<li><div class="kafel"><div class="wyp item-image <?php include ('losowanie2.php');?>"></div><div class="dymek">test</div></div></li>
+		</ul>
+		<div class="clear"></div>
+	</div>
+
+ 
+</BODY>
+</HTML
